203f485f2b76fa5728e38b33e49d301d37a39258
Ivan Storck <ivanoats@gmail.com>
2011-06-22 22:03:16 -0700

    blog post spec work

--- db/migrate/7_add_link_boolean_and_url_to_post.rb
+++ db/migrate/7_add_link_boolean_and_url_to_post.rb
@@ -1,6 +1,6 @@
-class AddLinkBookleanAndUrlToPost < ActiveRecord::Migration
+class AddLinkBooleanAndUrlToPost < ActiveRecord::Migration
   def self.up
-    add_column :blog_posts, :url, :string
+    add_column :blog_posts, :link_url, :string
     add_column :blog_posts, :is_link, :boolean
   end
 
