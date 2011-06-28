bae0ef873fdb35070dbfcdfd945ad62886d35c36
Ivan Storck <ivanoats@gmail.com>
2011-06-27 20:04:54 -0700

    removed extra spec

--- spec/views/shared/_post.html.erb_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "blog/shared/_post.html.erb" do
-  it "should let users select the blog post as a bookmark link" do
-   render
-   rendered.should contain("is a bookmark link?") # in the more options field
-   rendered.should contain #checkbox
-  end
-end
