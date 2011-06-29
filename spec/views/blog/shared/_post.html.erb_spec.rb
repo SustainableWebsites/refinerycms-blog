a65fcfe259b7479120723d859374dd0a5a91dcf9
Ivan Storck <ivanoats@gmail.com>
2011-06-29 12:22:47 -0700

    spec renders partial (based on http://relishapp.com/rspec/rspec-rails/v/2-6/dir/view-specs/view-spec )

--- spec/views/blog/shared/_post.html.erb_spec.rb
+++ spec/views/blog/shared/_post.html.erb_spec.rb
@@ -1,9 +1,16 @@
 require 'spec_helper'
 
 describe "blog/shared/_post.html.erb" do
+  let (:post) do
+    stub_model(BlogPost).as_new_record
+  end
+  
+  before do
+    assign(:post, post)
+  end
+  
   it "should let users mark the blog post as a bookmark link" do
-    assign(:post, mock_model("Post"))
-    render
+    render :partial => "blog/shared/post.html.erb", :locals => {:post => post}
     rendered.should contain("is a bookmark link?") # in the more options field
 #   rendered.should contain checkbox for is_link boolean
 #   rendered.should contain text field for link_url string
