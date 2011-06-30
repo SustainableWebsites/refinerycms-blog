65bd930fd2059f4053cae7c2599037d47ed13287
Ivan Storck <ivanoats@gmail.com>
2011-06-29 17:30:43 -0700

    admin view spec

--- spec/views/admin/blog/posts/edit.html.erb_spec.rb
+++ spec/views/admin/blog/posts/edit.html.erb_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
-describe 'admin/blog/posts/edit.html/erb' do
-  pending
+describe 'admin/blog/posts/new.html.erb' do
+  let (:post) do
+    stub_model(BlogPost).as_new_record
+  end
+  
+  before do
+    assign(:post, post)
+  end
+  
+  it "should let users mark the blog post as a bookmark link" do
+    render
+    rendered.should contain("is a bookmark link?") # in the more options field
+#   rendered.should contain checkbox for is_link boolean
+#   rendered.should contain text field for link_url string
+  end
 end
